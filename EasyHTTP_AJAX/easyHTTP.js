94f5af2510ccdf33b52bbf37fcc9ca0ccddc6c61
Hlsorrells <hlsorrells.dev@gmail.com>
2022-04-03 19:37:37 -0400

    Added PUT method to easyHTTP prototype

--- EasyHTTP_AJAX/easyHTTP.js
+++ EasyHTTP_AJAX/easyHTTP.js
@@ -46,6 +46,17 @@ easyHTTP.prototype.post = function (url, data, callback) {
 }
 
 // Make an HTTP PUT Request Method
+easyHTTP.prototype.put = function (url, data, callback) {
+    this.http.open('PUT', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+    let self = this;
+    this.http.onload = function () {
+        callback(null, self.http.responseText);
+    }
+
+    this.http.send(JSON.stringify(data));
+}
 
 // Make an HTTP DELETE Request Method
 
