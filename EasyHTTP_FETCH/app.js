418730bfa99491298e308053c7e32be08291cf8e
Hlsorrells <hlsorrells.dev@gmail.com>
2022-04-03 23:23:32 -0400

    Added GET method for json file

--- EasyHTTP_FETCH/app.js
+++ EasyHTTP_FETCH/app.js
@@ -1,5 +1,6 @@
 // Click listeners for each of the buttons
 document.getElementById('button1').addEventListener('click', getText);
+document.getElementById('button2').addEventListener('click', getJson);
 
 // Get local text file data
 function getText() {
@@ -10,6 +11,25 @@ function getText() {
         .then(function (data) {
             console.log(data);
             document.getElementById('output').innerHTML = data;
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+
+// Get local json data
+function getJson() {
+    fetch('posts.json')
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            let output = '';
+            data.forEach(function (post) {
+                output += `<li>${post.title}</li>`;
+            });
+            document.getElementById('output').innerHTML = output;
         })
         .catch(function (err) {
             console.log(err);
