85fa2c364f7f1e4a7ab3948c8d35846f0b6b75ac
Hlsorrells <hlsorrells.dev@gmail.com>
2022-04-03 23:54:53 -0400

    Added POST method & data to app

--- EasyHTTP_FETCH_ES6/app.js
+++ EasyHTTP_FETCH_ES6/app.js
@@ -1,7 +1,19 @@
 const http = new EasyHTTP;
 const url = 'https://jsonplaceholder.typicode.com';
 
+// User Data
+const data = {
+    name: 'John Doe',
+    username: 'johndoe',
+    email: 'jdoe@gmail.com'
+}
+
 // Get Users
 http.get(`${url}/users`)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+// Create User
+http.post(`${url}/users`, data)
     .then(data => console.log(data))
     .catch(err => console.log(err));
