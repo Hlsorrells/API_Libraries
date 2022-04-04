793a844d1f34c91fe2f412fe3cae9745ac7d6520
Hlsorrells <hlsorrells.dev@gmail.com>
2022-04-03 23:26:21 -0400

    Added GET method for external API

--- EasyHTTP_FETCH/app.js
+++ EasyHTTP_FETCH/app.js
@@ -1,6 +1,7 @@
 // Click listeners for each of the buttons
 document.getElementById('button1').addEventListener('click', getText);
 document.getElementById('button2').addEventListener('click', getJson);
+document.getElementById('button3').addEventListener('click', getExternal);
 
 // Get local text file data
 function getText() {
@@ -28,6 +29,25 @@ function getJson() {
             let output = '';
             data.forEach(function (post) {
                 output += `<li>${post.title}</li>`;
+            });
+            document.getElementById('output').innerHTML = output;
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+
+// Get from external API
+function getExternal() {
+    fetch('https://api.github.com/users')
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            let output = '';
+            data.forEach(function (user) {
+                output += `<li>${user.login}</li>`;
             });
             document.getElementById('output').innerHTML = output;
         })
